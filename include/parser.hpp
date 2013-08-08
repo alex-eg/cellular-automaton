e67592981dbf63646a586e58aaa188e39750dff5
alex_e <alexey.e.egorov@gmail.com>
2013-08-08 15:33:23 +0400

    working on .ini-like parser

--- include/parser.hpp
+++ include/parser.hpp
@@ -6,6 +6,21 @@
 #include <string>
 #include <map>
 
+typedef std::map <std::string, std::string> KeyValueStorage;
+
+class Settings {
+private:
+    std::map <std::string, KeyValueStorage> Sections;
+public:
+    void AddToSection(std::string section_name, std::string key, std::string value);
+    void SearchInSection(std::string item, std::string section = std::string(""));
+    void Clear();
+    
+    Settings() {}
+    ~Settings() {}
+
+};
+
 class Parser {
 private:
 public:
@@ -14,4 +29,5 @@ public:
     ~Parser() {}
     
 };
+
 #endif
--- src/parser.cpp
+++ src/parser.cpp
@@ -4,13 +4,13 @@ void Parser::Parse(std::string filename)
 {
     std::string line;
     std::fstream inifile(filename.c_str());
-    if (!inifile) std::cout<<"Cannot open file "<<filename<<std::endl;
+    if (!inifile) std::cout << "Cannot open file " << filename << std::endl;
     while (!inifile.eof()) {
-	std::getline(inifile, line); 
-	std::cout<<string;
-	if (!line.length()) continue;
-	if (line[0] == '#') continue;
-	if (line[0] == ';') continue;
+        std::getline(inifile, line); 
+        std::cout<<string;
+        if (!line.length()) continue;
+        if (line[0] == '#') continue;
+        if (line[0] == ';') continue;
     }
     inifile.close();
 }

22ba024c7df8730d10b9267670b147c03e74b17b
alex_e <alexey.e.egorov@gmail.com>
2013-08-09 16:46:05 +0400

    improved parser_test program with new parser functionality

--- src/parsertest.cpp
+++ src/parsertest.cpp
@@ -1,11 +1,15 @@
 #include "parser.hpp"
 #include <cstdlib>
+#include <iostream>
 
 int main(int argc, char **argv)
 {
-    if (argc<2) exit(1);
-    std::string in = argv[1];
+    if (argc < 2) exit(1);
+    std::string in(argv[1]);
     Parser P;
-    P.Parse(in);
+    Settings S;
+    S = P.Parse(in);
+    std::cout << "\n\n\n";
+    std::cout << S.Find("Wolol");
     exit(0);
 }
